10b7493ae053b7e15d952cd52f7c4578c6e7ee04
Gustavo  Barros <gustavoandrade0125@gmail.com>
2024-02-15 13:04:23 -0300

    mudancas 2

--- function.js
+++ function.js
@@ -1,12 +1,6 @@
 
 function soma(){
     var input = document.getElementById("number")
-    var roda = document.querySelectorAll(".text")
-
-    roda.forEach( function(element){
-        element.style.animation = "curveText 1s infinite"
-    })
-
     newInput = parseInt(input.value) + 1;
     input.value = newInput
 }
